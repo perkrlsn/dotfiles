09046dfb40eccf6e069ca5f8af8df35eb40326c0
perkrlsn <per.karlsson@meltwater.com>
2018-04-27 20:47:29 +0200

    added iterm2

--- templates/provisioning/mac/macos.sh
+++ templates/provisioning/mac/macos.sh
@@ -716,6 +716,9 @@ sudo mdutil -E / > /dev/null
 # Only use UTF-8 in Terminal.app
 defaults write com.apple.terminal StringEncodings -array 4
 
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/templates/iterm2"
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
 # Enable “focus follows mouse” for Terminal.app and all X11 apps
 # i.e. hover over a window and start typing in it without clicking first
 #defaults write com.apple.terminal FocusFollowsMouse -bool true
