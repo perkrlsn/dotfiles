776b9b2d88c53ff3b2555375b104803ff33b3659
perkrlsn <per.karlsson@meltwater.com>
2018-04-27 19:20:39 +0200

    updated

--- bash/plugins/rvm.plugin.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-
-if [ "$rvm_path" ]; then
-    [[ -r "$rvm_path"/scripts/completion ]] && . "$rvm_path"/scripts/completion
-fi
--- bash/plugins/tmux.plugin.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# make sure that tmux is launched in 256 color mode
-alias tmux="TERM=xterm-256color tmux -u"
-
-# load tmuxinator if installed
-[[ -s "$HOME/.tmuxinator/scripts/tmuxinator" ]] && . "$HOME/.tmuxinator/scripts/tmuxinator"
