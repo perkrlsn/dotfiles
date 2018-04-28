f039f8009eb8c70177a396447caa60411a0b5769
perkrlsn <per.karlsson@me.com>
2018-04-28 09:43:43 +0200

    mas fix

--- templates/provisioning/mac/appstore.sh
+++ templates/provisioning/mac/appstore.sh
@@ -4,4 +4,4 @@ echo "Installing Mac AppSore Apps"
 echo "==========================="
 while read -r in; do
   mas install "$in"
-done < <(sed -E 's/#.+//g' templates/dependencies/appstore-apps)
+done < <(sed -E 's/#.+//g' "$HOME"/dotfiles/templates/dependencies/appstore-apps)
